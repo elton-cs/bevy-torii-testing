80765b73ecc8af19077c3f3832b437d4ce6ffd04
elton-cs <govada.elton@gmail.com>
2024-08-11 19:20:13 -0400

    Implement default camera view

--- bevy-frontend/src/main.rs
+++ bevy-frontend/src/main.rs
@@ -1,9 +1,17 @@
 use bevy::prelude::*;
-use bevy_frontend::plugins::torii::ToriiPlugin;
+use bevy_frontend::plugins::{torii::ToriiPlugin, visualize::VisualizePlugin};
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
+    app.add_systems(Startup, default_camera);
     app.add_plugins(ToriiPlugin);
+    app.add_plugins(VisualizePlugin);
     app.run();
+}
+
+fn default_camera(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scale = 0.3;
+    commands.spawn(camera_bundle);
 }
